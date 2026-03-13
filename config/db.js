be3bba4988443a8020ed994e9b7b24fe7da29967
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 21:37:52 +0530

    Add some features

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("Database connection error:", error);
+    process.exit(1);
   }
 };
 
