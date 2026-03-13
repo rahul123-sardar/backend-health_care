f5ec466750e0e9a236911dfcf89beeeb2761397c
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 21:03:43 +0530

    Add some features

--- config/multer.js
+++ config/multer.js
@@ -1,19 +1,15 @@
 import multer from "multer";
-import  CloudinaryStorage  from "multer-storage-cloudinary";
-import cloudinary from "./cloudinary.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
     folder: "patients",
-    allowed_formats: ["jpg", "png", "jpeg", "avif", "webp"]
+    allowed_formats: ["jpg", "png", "jpeg"]
   }
 });
 
 const upload = multer({ storage });
-
-
-
-
 
 export default upload;
