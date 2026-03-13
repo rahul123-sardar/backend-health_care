07aaa02212c64e8c33b5b2bad76fb0a40d02a329
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-14 01:59:21 +0530

    Add some features

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ import patientRoutes from "./routes/patientRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-health-care-pink.vercel.app", // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
