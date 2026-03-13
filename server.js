830d34483245e5d894de116a2fb04086fa733e3c
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 05:51:43 +0530

    Add some features

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-import patientRoutes from "./routes/patientRoutes.js";
+import "dotenv/config";
 
-dotenv.config();
+import patientRoutes from "./routes/patientRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
-
 app.use("/api/patient", patientRoutes);
-app.use((err, req, res, next) => {
-  console.error("GLOBAL ERROR:", err);
-  res.status(500).json({ message: err.message });
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log("MongoDB Error:", err);
+});
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected successfully");
 });
 
-const PORT = 5000;
-
-// app.listen(PORT,()=>{
-//     console.log(`Server running on ${PORT}`);
-// });
-
-
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error:", err);
+});
 app.get("/", (req, res) => {
-  res.send("API Running");
+    res.send("API Running");
 });
 
 export default app;
