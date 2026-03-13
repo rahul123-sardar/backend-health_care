46c3513b9047c4ad18ac40d24a0015cf45c5956c
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 08:48:54 +0530

    Add some features

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -9,7 +9,9 @@ const router = express.Router();
 router.post("/save", upload.single("image"), async (req, res) => {
   try {
     console.log("BODY:", req.body);
-    console.log("FILE:", req.file); // contains Cloudinary info
+    console.log("FILE:", req.file);
+
+    const imageUrl = req.file ? req.file.path : "";
 
     const patient = await Patient.create({
       patientId: req.body.patientId,
@@ -18,15 +20,16 @@ router.post("/save", upload.single("image"), async (req, res) => {
       billingCode: req.body.billingCode,
       diagnosis: req.body.diagnosis,
       notes: req.body.notes,
-      image: req.file?.path || "", // Cloudinary URL
+      image: imageUrl
     });
 
     res.status(201).json({
       message: "Patient saved successfully",
       data: patient
     });
+
   } catch (error) {
-    console.error("ERROR:", error);
+    console.log("ERROR:", error);
     res.status(500).json({
       message: error.message,
       error: error
