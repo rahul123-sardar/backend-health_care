0e4e25438af8662c5fa267717fc9639b210dc5e2
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 10:23:13 +0530

    Add some features

--- server.js
+++ server.js
@@ -7,35 +7,28 @@ import patientRoutes from "./routes/patientRoutes.js";
 
 const app = express();
 
+// CORS Middleware (must be BEFORE routes)
 app.use(cors({
-  origin: ["http://localhost:5173", "https://frontend-health-care-pink.vercel.app"], // frontend URLs
-  methods: ["GET","POST","PUT","DELETE"],
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-health-care-pink.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/patient", patientRoutes);
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log("MongoDB Error:", err);
-});
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected successfully");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log("MongoDB connection error:", err);
-});
 app.get("/", (req, res) => {
     res.send("API Running");
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("MongoDB Error:", err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
-export default app;
