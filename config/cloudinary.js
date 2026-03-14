112054ab0fd22eb00e0eb38769181ffdedda7cc7
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-14 17:59:22 +0530

    Add some features

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 export default cloudinary;
