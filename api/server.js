7109cb4ac0e19cb0e5634d68139b6623e791b43b
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-14 22:23:18 +0530

    Add some features

--- api/server.js
+++ api/server.js
@@ -22,3 +22,6 @@ app.use("/api/patient", patientRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+export default app;
