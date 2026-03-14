ddc6fa5fc4d2e403d599447c35d2d76fbf915e7e
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-15 03:20:41 +0530

    Add some features

--- api/proxy-patient.js
+++ api/proxy-patient.js
@@ -1,19 +1,19 @@
-// frontend/api/proxy-patient.js
 import axios from "axios";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") return res.status(200).end();
-
   try {
     const response = await axios({
       method: req.method,
-      url: "https://backend-health-care-97bf.vercel.app/api/patient",
+      url: "https://backend-health-care-wrp.vercel.app/api/patient",
       data: req.body,
+      headers: req.headers,
     });
+
+    // Allow CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     res.status(200).json(response.data);
   } catch (err) {
     console.error(err);
